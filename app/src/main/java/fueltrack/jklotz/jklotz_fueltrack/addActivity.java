b7db0680c11467dbfc092f26764fce9828abbe8e
Jessica Klotz <Jessica@Jessicas-MacBook-Pro.local>
2016-01-18 23:59:11 -0700

    First Commit Main Screen with buttons for adding and viewing/editing When add button is click go to new screen

---
+++ app/src/main/java/fueltrack/jklotz/jklotz_fueltrack/addActivity.java
@@ -0,0 +1,26 @@
+package fueltrack.jklotz.jklotz_fueltrack;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class addActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+}
